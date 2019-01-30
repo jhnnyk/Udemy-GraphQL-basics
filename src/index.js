76c1692337b4f283ca1bfba9fbf8c6904e6c622d
John Knotts <jhnnyk@gmail.com>
2019-01-29 20:11:31 -0700

    ES6 import/export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello GraphQL');
+import myCurrentLocation, { getGreeting, message, myName } from './myModule';
+import addTwoNumbers, { subtractNumbers } from './math';
+
+console.log(message);
+console.log(myName);
+console.log(myCurrentLocation);
+console.log(getGreeting('Travis'));
+
+console.log(addTwoNumbers(5, 10));
+console.log(subtractNumbers(20, 12));
---
+++ src/math.js
@@ -0,0 +1,9 @@
+const addNumbers = (a, b) => {
+  return a + b;
+};
+
+const subtractNumbers = (c, d) => {
+  return c - d;
+};
+
+export { subtractNumbers, addNumbers as default };
